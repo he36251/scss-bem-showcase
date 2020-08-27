46e33eba7ed779ae45402727b69fcf36a22e3d9e
Gawo <gawo.he@gmail.com>
2020-08-27 23:23:48 +1200

    Initial files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import './styles/main.scss';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <div className="home">
+                    <h3 className="home__title">This is a test title</h3>
+                    <h3 className="home__title home__title--danger">This is a test title</h3>
+                </div>
+            </header>
+        </div>
+    );
 }
 
 export default App;
